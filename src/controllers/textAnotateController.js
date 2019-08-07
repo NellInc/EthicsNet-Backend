1607522efc0310c1a5238834a2a746b850d25025
Emerson Lopes <lupuselit@outlook.com>
2019-08-07 10:48:44 -0300

    add user routes

--- src/controllers/textAnotateController.js
+++ src/controllers/textAnotateController.js
@@ -8,7 +8,7 @@ router.get('/post-text', (req, res) => {
   res.send({
     ok: true,
     user: req.user,
-    bla: req.bla,
+    bla: req.bla, 
   });
 });
 
@@ -16,12 +16,6 @@ router.post('/post-text', async (req, res) => {
   console.log('post text route *_*');
 
   try {
-    // res.send({
-    //   ok: true,
-    //   user: req.user,
-    //   bla: req.bla,
-    // });
-
     console.log('\n\ntext to data -> ', req.body);
 
     const textCreated = await TextAnotation.create(req.body);
@@ -31,7 +25,45 @@ router.post('/post-text', async (req, res) => {
     //
   } catch (error) {
     console.log(error);
+    return res.status(500).send({ error: 'internal server error' });
+  }
+});
 
+router.get('/user', async (req, res) => {
+  try {
+    const user = await User.findById(req.userId);
+
+    console.log(req.user);
+
+    return res.status(200).send({ message: 'user found!', user });
+
+    //
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({ error: 'internal server error' });
+  }
+});
+
+router.get('/user/anotations', async (req, res) => {
+  try {
+
+    // const user = await User.findById(req.userId);
+
+    // console.log(req.user);
+
+    // return res.status(200).send({ message: 'user found!', user });
+
+    console.log('user id -> ', req.userId);
+    
+    const anotations = await TextAnotation.find({
+      authorId: req.userId
+    })
+
+    return res.status(200).send({ anotations })
+
+    //
+  } catch (error) {
+    console.log(error);
     return res.status(500).send({ error: 'internal server error' });
   }
 });
