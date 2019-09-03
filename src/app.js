3f1ca0177174dbe734c6152fa04d369b1006df3b
Emerson Lopes <lupuselit@outlook.com>
2019-09-02 23:23:27 -0300

    change port

--- src/app.js
+++ src/app.js
@@ -29,4 +29,4 @@ app.get('/', (req, res) => {
 console.log('\n\n hello world!');
 
 // TODO: change this to 5000 when on deployment
-app.listen(80, () => console.log('api is working usually on port 5000'));
+app.listen(5000, () => console.log('api is working usually on port 5000'));
